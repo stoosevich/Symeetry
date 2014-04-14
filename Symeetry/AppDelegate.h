f9a74ebecfd22a89d3402653ee8ba41bcfda9b08
Steve Toosevich <stoosevich@gmail.com>
2014-04-14 16:07:24 -0500

    Initial Commit

---
+++ Symeetry/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Symeetry
+//
+//  Created by Steve Toosevich on 4/14/14.
+//  Copyright (c) 2014 Steve Toosevich. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Symeetry/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Symeetry
+//
+//  Created by Steve Toosevich on 4/14/14.
+//  Copyright (c) 2014 Steve Toosevich. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
