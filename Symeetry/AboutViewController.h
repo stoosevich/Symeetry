eb0245ae499ac94a6c61124884c6260ca4f941c7
dkw5877 <dkw5877@gmail.com>
2014-04-29 15:09:00 -0500

    add About view controller

---
+++ Symeetry/AboutViewController.h
@@ -0,0 +1,13 @@
+//
+//  AboutViewController.h
+//  Symeetry
+//
+//  Created by user on 4/29/14.
+//  Copyright (c) 2014 Steve Toosevich. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AboutViewController : UIViewController
+
+@end
