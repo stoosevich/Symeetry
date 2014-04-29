4428aa143814b34442daf3e810f9e0a3ba12946f
Steve Toosevich <stoosevich@gmail.com>
2014-04-28 22:17:49 -0500

    Camera update

---
+++ Symeetry/PhotoViewController.h
@@ -0,0 +1,13 @@
+//
+//  PhotoViewController.h
+//  Symeetry
+//
+//  Created by Steve Toosevich on 4/28/14.
+//  Copyright (c) 2014 Steve Toosevich. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PhotoViewController : UIViewController
+
+@end
