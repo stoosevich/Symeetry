330900b1af26f2bac92ce01162aa66b8a00958a6
Steve Toosevich <stoosevich@gmail.com>
2014-04-30 16:34:32 -0500

    Added Edit profile View

---
+++ Symeetry/EditProfileViewController.h
@@ -0,0 +1,13 @@
+//
+//  EditProfileViewController.h
+//  Symeetry
+//
+//  Created by Steve Toosevich on 4/30/14.
+//  Copyright (c) 2014 Steve Toosevich. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EditProfileViewController : UIViewController
+
+@end
