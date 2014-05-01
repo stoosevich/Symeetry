244dc40a6486b5f7b40482448af2ba3d6fcd3f7a
Charles Northup <cjnorth40@gmail.com>
2014-04-30 23:42:18 -0500

    fixed bug

--- CreateNewUserViewController.m
+++ CreateNewUserViewController.m
@@ -39,6 +39,8 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    self.passwordTextField.secureTextEntry = YES;
+    self.comfirmpasswordTextField.secureTextEntry = YES;
     
     // Do any additional setup after loading the view.
 }
@@ -90,6 +92,7 @@
 - (IBAction)onSignUpButtongPressed:(id)sender
 {
     self.signUpButton.enabled = NO;
+    self.signUpButton.alpha = 0.4;
     if ((![self.usernameTextField.text isEqualToString:@""])&&
         (![self.passwordTextField.text isEqualToString:@""])&&
         ([self.passwordTextField.text isEqualToString:self.comfirmpasswordTextField.text])&&
@@ -99,63 +102,69 @@
         [newUser setPassword:self.passwordTextField.text];
         [newUser setUsername:self.usernameTextField.text];
         [newUser setEmail:self.emailTextField.text];
-        [newUser setObject:@(0) forKey:@"similarityIndex"];
-        [newUser setObject:[ParseManager convertUIImageToPFFile:[UIImage imageNamed:@"ic_welcome_profile.png"]] forKey:@"photo"];
-        [newUser setObject:[ParseManager convertUIImageToPFFile:[Utilities resizeImage:[UIImage imageNamed:@"ic_welcome_profile.png"] withWidth:40 andHeight:40]] forKey:@"thumbnail"];
-        [newUser setObject:@NO forKey:@"gender"];
-        [newUser setObject:@(0) forKey:@"age"];
-        [newUser setObject:@"" forKey:@"biography"];
-        [newUser setObject:@NO forKey:@"hidden"];
-        [newUser setObject:@"NOT_INITIALIZED" forKey:@"nearestBeacon"];
+//        [newUser setObject:@(0) forKey:@"similarityIndex"];
+//        [newUser setObject:[ParseManager convertUIImageToPFFile:[UIImage imageNamed:@"ic_welcome_profile.png"]] forKey:@"photo"];
+//        [newUser setObject:[ParseManager convertUIImageToPFFile:[Utilities resizeImage:[UIImage imageNamed:@"ic_welcome_profile.png"] withWidth:40 andHeight:40]] forKey:@"thumbnail"];
+//        [newUser setObject:@NO forKey:@"gender"];
+//        [newUser setObject:@(0) forKey:@"age"];
+//        [newUser setObject:@"" forKey:@"biography"];
+//        [newUser setObject:@NO forKey:@"hidden"];
+//        [newUser setObject:@"NOT_INITIALIZED" forKey:@"nearestBeacon"];
         
         dispatch_async(dispatch_get_main_queue(), ^{
-            NSError* error = [NSError new];
-            [newUser signUp:&error];
-            
-            if (error)
-            {
-                if ([error code] == 202)
-                {
-                    [self raiseUserNameTakenAlert];
+            [newUser signUpInBackgroundWithBlock:^(BOOL succeeded, NSError *error) {
+                if (succeeded) {
+                    [newUser setObject:@(0) forKey:@"similarityIndex"];
+                   // [newUser setObject:[ParseManager convertUIImageToPFFile:[Utilities resizeImage:[UIImage imageNamed:@"ic_welcome_profile.png"] withWidth:40 andHeight:40]] forKey:@"thumbnail"];
+                    [newUser setObject:@NO forKey:@"gender"];
+                    [newUser setObject:@(0) forKey:@"age"];
+                    [newUser setObject:@"" forKey:@"biography"];
+                    [newUser setObject:@NO forKey:@"hidden"];
+                    [newUser setObject:@"NOT_INITIALIZED" forKey:@"nearestBeacon"];
+                    [newUser saveInBackgroundWithBlock:^(BOOL succeeded, NSError *error) {
+                        if (succeeded) {
+                            PFObject* interest = [PFObject objectWithClassName:@"Interests"];
+                            [interest setObject:newUser.objectId forKey:@"userid"];
+                            [interest setObject:newUser forKey:@"user"];
+                            [interest setObject:@(0) forKey:@"movies"];
+                            [interest setObject:@(0) forKey:@"music"];
+                            [interest setObject:@(0) forKey:@"food"];
+                            [interest setObject:@(0) forKey:@"school"];
+                            [interest setObject:@(0) forKey:@"dancing"];
+                            [interest setObject:@(0) forKey:@"books"];
+                            [interest setObject:@(0) forKey:@"tv"];
+                            [interest setObject:@(0) forKey:@"art"];
+                            [interest setObject:@(0) forKey:@"technology"];
+                            [interest setObject:@(0) forKey:@"games"];
+                            [interest setObject:@(0) forKey:@"fashion"];
+                            [interest setObject:@(0) forKey:@"volunter"];
+                            [interest saveInBackgroundWithBlock:^(BOOL succeeded, NSError *error) {
+                                if (succeeded) {
+                                    [newUser setObject:interest forKey:@"interests"];
+                                    [newUser saveInBackgroundWithBlock:^(BOOL succeeded, NSError *error) {
+                                        if (succeeded) {
+                                            [newUser setObject:[ParseManager convertUIImageToPFFile:[UIImage imageNamed:@"ic_welcome_profile.png"]] forKey:@"photo"];
+                                            NSLog(@"sined up");
+                                            self.signedUp = YES;
+                                            UIAlertView* created = [[UIAlertView alloc] initWithTitle:@"Welcome" message:@"Your account has been created succesfully" delegate:self cancelButtonTitle:@"Done" otherButtonTitles:nil];
+                                            [created show];
+                                            self.signUpButton.hidden = YES;
+                                            [[PageViewController sharedPageViewController] signUpWasSuccesful];
+                                            [newUser saveInBackground];
+                                            [newUser setObject:[ParseManager convertUIImageToPFFile:[Utilities resizeImage:[UIImage imageNamed:@"ic_welcome_profile.png"] withWidth:40 andHeight:40]] forKey:@"thumbnail"];
+                                            [newUser saveInBackground];
+                                        }
+                                    }];
+                                }
+                            }];
+                        }
+                    }];
                 }
-                
-                else
-                {
-                    [self raiseParseErrorAlert:error];
-                }
+            }];
 
-            }
-            else if(!error)
-            {
-                PFObject* interest = [PFObject objectWithClassName:@"Interests"];
-                [interest setObject:newUser.objectId forKey:@"userid"];
-                [interest setObject:newUser forKey:@"user"];
-                [interest setObject:@(0) forKey:@"movies"];
-                [interest setObject:@(0) forKey:@"music"];
-                [interest setObject:@(0) forKey:@"food"];
-                [interest setObject:@(0) forKey:@"school"];
-                [interest setObject:@(0) forKey:@"dancing"];
-                [interest setObject:@(0) forKey:@"books"];
-                [interest setObject:@(0) forKey:@"tv"];
-                [interest setObject:@(0) forKey:@"art"];
-                [interest setObject:@(0) forKey:@"technology"];
-                [interest setObject:@(0) forKey:@"games"];
-                [interest setObject:@(0) forKey:@"fashion"];
-                [interest setObject:@(0) forKey:@"volunter"];
-                [interest saveInBackgroundWithBlock:^(BOOL succeeded, NSError *error) {
-                    
-                }];
-                NSLog(@"sined up");
-                self.signedUp = YES;
-                UIAlertView* created = [[UIAlertView alloc] initWithTitle:@"Welcome" message:@"Your account has been created succesfully" delegate:self cancelButtonTitle:@"Done" otherButtonTitles:nil];
-                [created show];
-                self.signUpButton.hidden = YES;
-                [[PageViewController sharedPageViewController] signUpWasSuccesful];
-            }
         });
-
-
     }
+    self.signUpButton.alpha = 1.0;
     self.signUpButton.enabled = YES;
 }
 
